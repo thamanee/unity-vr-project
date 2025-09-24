3b7bc42e414652939f62ea2e07747436768fbd5c
thamanee <102239415+thamanee@users.noreply.github.com>
2025-09-24 16:30:13 +0700

    InClassWeek6

---
+++ Assets/GI479Assets/Week6/Scripts/Health.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100;
+
+    public UnityEvent OnTakeDamage;
+    public UnityEvent OnDead;
+
+    private float currentHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        if (!IsDead())
+        {
+            OnTakeDamage.Invoke();
+            currentHealth -= damageAmount;
+
+            if (IsDead())
+            {
+                OnDead.Invoke();
+            }
+        }
+    }
+
+    public bool IsDead()
+    {
+        return currentHealth <= 0;
+    }
+}
